6aa54695d97821e423654b23b5b0bb644e6cb5dc
bimane1 <barkat7imane7@gmail.com>
2025-07-07 10:26:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-// معاينة صورة التحميل
-const imageInput = document.getElementById('imageInput');
-const previewImage = document.getElementById('previewImage');
-
-if (imageInput && previewImage) {
-  imageInput.addEventListener('change', function () {
-    const file = this.files[0];
-    if (file && file.type.startsWith('image/')) {
-      const reader = new FileReader();
-      reader.onload = function (e) {
-        previewImage.src = e.target.result;
-        previewImage.style.display = 'block';
-      };
-      reader.readAsDataURL(file);
-    }
-  });
-}
-
 // إرسال نموذج الاتصال
 const contactForm = document.getElementById('contactForm');
 if (contactForm) {
